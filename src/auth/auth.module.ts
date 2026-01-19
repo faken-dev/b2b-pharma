b0ec4663e6863277564748132767e02a443c934c
Faken <faken.dev@gmail.com>
2026-01-19 20:50:35 +0700

    feat(auth): wire AuthModule with JwtModule, NotificationModule, controller & service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { NotificationModule } from 'src/notification/notification.module';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { NotificationModule } from '../notification/notification.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), NotificationModule],
-  controllers: [],
-  providers: [],
-  exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    NotificationModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET') ?? 'default_jwt_secret',
+        signOptions: { expiresIn: '24h' },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
